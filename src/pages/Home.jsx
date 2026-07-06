d4b30fc5367e14de5f41551c43c458f3d63668bb
byun970 <byun970@gmail.com>
2026-07-06 16:09:30 +0900

    Home 페이지 UI 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
 import Button from "../components/Button";
+import DiaryList from "../components/DiaryList";
 
 const Home = () => {
   return (
     <div>
-      <Header title={"2024년 4월"} />
+      <Header title={"2024년 4월"} leftChild={<Button text={"<"} />} rightChild={<Button text={">"} />} />
+      <DiaryList />
     </div>
   );
 };
